2e99c618d12114723abff50b13115a221dc03df8
Shipad Ambore <shripadambore01@gmail.com>
2026-08-21 00:33:13 +0530

    Add default Vercel serverless entrypoint api/index.py

---
+++ api/index.py
@@ -0,0 +1,8 @@
+﻿import sys
+from pathlib import Path
+
+# Add project root to sys.path
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
+
+from backend.main import app
